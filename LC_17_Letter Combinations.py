c55ab16dbd0f393b2a7920ed9d5b5c107e59f6e4
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-06-27 23:37:38 -0700

    Update LC_17_Letter Combinations.py

--- LC_17_Letter Combinations.py
+++ LC_17_Letter Combinations.py
@@ -1,3 +1,30 @@
+"""
+Time Complexity = O(n^4)
+Sapce Complexity = O(n)
+
+a in abc
+    curr = a
+    helper(a, 1)
+        d in def
+            curr = ad
+            helper(ad, 2): exit out
+            curr = a
+        e in def
+            curr = ae
+            helper(ae, 2): exit out
+            curr = a
+        f in def
+            curr = af
+            helper(af, 2): exit out
+            curr = a
+        curr = ''
+
+b in abc
+    ....
+    ....
+    ....
+"""
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
